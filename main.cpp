a15ae3e37033dd3c262cc6c3b6d085a2b18a0b99
y <yvain29@gmail.com>
2021-01-03 11:56:14 +0100

    fix starting value for outer loop in raw mode

--- main.cpp
+++ main.cpp
@@ -115,7 +115,7 @@ int main(int argc, char *argv[]) {
 	    else {
 		    generateur->L = 0;
 		    generateur->A = 0;
-		    generateur->X = 0;
+		    generateur->X = 1;
 		    int mmm = generateur->max-generateur->min;
 		    generateur->arrayofindex = new int *[mmm+1];
 		    for(int a=0; a<=mmm; ++a) {
